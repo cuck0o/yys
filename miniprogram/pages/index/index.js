3a633ca42adbbaa9bafe4b53ae27604ec664f9c2
lijian <lijian_316@hotmail.com>
2020-10-31 11:16:57 +0800

    11

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,7 +9,13 @@ Page({
     next1: true,
     next2: true,
     next3: true,
-    next4: true
+    next4: true,
+    position1: -140,
+    position2: -125,
+    position3: -145,
+    position4: -146,
+    userInfo: null,
+    curTouch: -1
   },
 
   onLoad: function () {
@@ -18,25 +24,25 @@ Page({
       duration: 0,
       delay: 500
     });
-    animation1.left(-143).step();
+    animation1.left(this.data.position1).step();
     // 动画2
     var animation2 = wx.createAnimation({
       duration: 0,
       delay: 500
     });
-    animation2.left(-152).step();
+    animation2.left(this.data.position2).step();
     // 动画3
     var animation3 = wx.createAnimation({
       duration: 0,
       delay: 500
     });
-    animation3.left(-166).step();
+    animation3.left(this.data.position3).step();
     // 动画4
     var animation4 = wx.createAnimation({
       duration: 0,
       delay: 500
     });
-    animation4.left(-172).step();
+    animation4.left(this.data.position4).step();
 
     this.setData({
       ani1: animation1.export(),
@@ -56,7 +62,7 @@ Page({
       delay: 500
     });
     if (this.data.next1) {
-      animation.left(-143).step();
+      animation.left(this.data.position1).step();
     } else {
       animation.left(0).step();
     }
@@ -72,7 +78,7 @@ Page({
       delay: 500
     });
     if (this.data.next2) {
-      animation.left(-152).step();
+      animation.left(this.data.position2).step();
     } else {
       animation.left(0).step();
     }
@@ -88,7 +94,7 @@ Page({
       delay: 500
     });
     if (this.data.next3) {
-      animation.left(-166).step();
+      animation.left(this.data.position3).step();
     } else {
       animation.left(0).step();
     }
@@ -104,7 +110,7 @@ Page({
       delay: 500
     });
     if (this.data.next4) {
-      animation.left(-172).step();
+      animation.left(this.data.position4).step();
     } else {
       animation.left(0).step();
     }
@@ -112,5 +118,179 @@ Page({
       ani4: animation.export(),
       next4: !this.data.next4
     })
-  }
+  },
+
+  checkInfo(e) {
+    this.setData({
+      curTouch: e.currentTarget.dataset['index']
+    })
+    // 用户登录信息（openid获取，基础数据获取，活动数据获取）
+    wx.getSetting({
+      onGetUserInfo: function (e) {
+        if (!this.data.logged && e.detail.userInfo) {
+          this.setData({
+            logged: true,
+            userInfo: e.detail.userInfo
+          })
+        }
+      },
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log("getUserInfo:", res.userInfo);
+              this.setData({
+                userInfo: res.userInfo,
+              })
+            }
+          })
+        }
+      }
+    })
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        app.globalData.openid = res.result.openid;
+        this.getUserData();
+      },
+      fail: err => {
+        this.showErr("获取用户信息出错，请刷新小程序后重试");
+      }
+    })
+  },
+
+  getUserData: function () {
+    var that = this;
+    const db = wx.cloud.database({
+      env: 'yys-7gws87sn973c67e2'
+    })
+    db.collection('yys').where({
+      "_openid": app.globalData.openid
+    }).get({
+      success: function (res) {
+        if (res.data.length > 0) {
+          console.log("getUserData:", res.data);
+          app.globalData.userObj = res.data[0];
+          that.goto();
+        } else {
+          that.createUserData();
+        }
+      },
+      fail: function (err) {
+        wx.showModal({
+          title: 'system error',
+          content: "获取用户数据失败，请刷新后重试",
+        })
+        console.log(err);
+      }
+    })
+  },
+
+  // 新建用户开放信息
+  createUserData: function () {
+    const db = wx.cloud.database({
+      env: 'yys-7gws87sn973c67e2'
+    })
+    var that = this;
+    var data = {
+      card1: {
+        time: 0,
+        need: [],
+        give: [],
+      },
+      card2: {
+        time: 0,
+        need: [],
+        give: [],
+      },
+      ssr: {
+        time: 0,
+        need: [],
+        give: [],
+      },
+      task: []
+    }
+    db.collection('yys').add({
+      data: data,
+      success: function (res) {
+        console.log("createUserData:", res);
+        app.globalData.userObj = data;
+        that.goto();
+      },
+      fail: function (err) {
+        console.log(err);
+      }
+    })
+  },
+
+  goto() {
+    console.log("here i am", this.data.curTouch, app.globalData.userObj);
+    if (this.data.curTouch != -1) {
+      this.setData({
+        curTouch: -1
+      })
+      var curtime = Date.parse(new Date()) / 1000;
+      switch (index) {
+        case 1:
+          if (app.globalData.userObj.card1.time == 0 || app.globalData.userObj.card1.time <= curtime) {
+            this.creatCard1();
+          } else {
+            this.toCard1();
+          }
+          break;
+        case 2:
+          if (app.globalData.userObj.card2.time == 0 || app.globalData.userObj.card2.time <= curtime) {
+            this.creatCard2();
+          } else {
+            this.toCard2();
+          }
+          break;
+        case 3:
+          if (app.globalData.userObj.ssr.time == 0 || app.globalData.userObj.ssr.time <= curtime) {
+            this.creatSsr();
+          } else {
+            this.toSsr();
+          }
+          break;
+        case 4:
+          if (app.globalData.userObj.task.length <= 0) {
+            this.createTask();
+          } else {
+            this.toTask();
+          }
+          break;
+      }
+    }
+  },
+
+  createCard1() {
+
+  },
+
+  toCard1() {
+    wx.navigateTo({
+      url: '/pages/card1/card1'
+    })
+  },
+
+  toCard2() {
+    wx.navigateTo({
+      url: '/pages/card1/card2'
+    })
+  },
+
+  toSsr() {
+    wx.navigateTo({
+      url: '/pages/ssr/ssr'
+    })
+  },
+
+  toTask() {
+    wx.navigateTo({
+      url: '/pages/task/task'
+    })
+  },
+
 })
